47e5cb27028932e22d0d254ed4b88f0b427d9d38
Rafael Macedo <macedorafael2002@gmail.com>
2023-06-13 12:11:54 -0300

    Projeto do curso de angular finalizado

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';
+
+@Component({
+  selector: 'app-quizz',
+  templateUrl: './quizz.component.html',
+  styleUrls: ['./quizz.component.css'],
+})
+export class QuizzComponent {
+  title: string = '';
+  questions: any = [];
+  selectedQuestion: any = '';
+
+  answers: string[] = [];
+  selectedAnswer: string = '';
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
+
+      this.questions = quizz_questions.questions;
+      this.selectedQuestion = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  playerChoice(value: string) {
+    this.answers.push(value);
+    this.nextStep();
+  }
+
+  async nextStep() {
+    this.questionIndex += 1;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.selectedQuestion = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer: string = await this.checkResult(this.answers);
+      this.finished = true;
+      this.selectedAnswer =
+        quizz_questions.results[
+          finalAnswer as keyof typeof quizz_questions.results
+        ];
+    }
+  }
+
+  async checkResult(answers: string[]) {
+    const result = answers.reduce((previous, current, index, arr) => {
+      if (
+        arr.filter((item) => item === previous).length >
+        arr.filter((item) => item === current).length
+      ) {
+        return previous;
+      } else {
+        return current;
+      }
+    });
+
+    return result;
+  }
+}
